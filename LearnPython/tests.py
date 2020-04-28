fe2186070c5bdd5c2f2a67c0fb5d2c1473152efe
Kingfish404 <jinyu929@qq.com>
2020-04-29 01:22:29 +0800

    update css 0.0.2

--- LearnPython/tests.py
+++ LearnPython/tests.py
@@ -6,8 +6,8 @@ from .models import timeOut
 
 class modelsTest(TestCase):
     def test_self(self):
-        code='1+1'
-        result='2'
+        code='2**10'
+        result='1024'
         self.assertEqual(run_code(code), result)
 
     def test_cmd(self):
@@ -19,21 +19,8 @@ class modelsTest(TestCase):
     def test_empty(self):
         self.assertEqual(run_code('  '), '请输入代码')
 
-
-
-    # def test_timeout(self):
-    #     code="from timeit import repeat\ndef func():\n\
-    #         s = 0\nfor i in range(10000):\n\
-    #         t = repeat('func()', 'from __main__ import func', number=100, repeat=500)\n"
-    #     self.assertEqual(run_code(code), '计算超时,请简化你的代码\n运行时间不得超过 '+str(timeOut)+' 秒')
-
-# from timeit import repeat
-
-# def func():
-#     s = 0
-#     for i in range(1000):
-#         s += i
-# #repeat和timeit用法相似，多了一个repeat参数，表示重复测试的次数(可以不写，默认值为3.)，返回值为一个时间的列表。
-# t = repeat('func()', 'from __main__ import func', number=100, repeat=500)
-# print(t) 
-# print(min(t))
+    def test_timeout(self):
+        code="from timeit import repeat\ndef func():\n\
+            s = 0\nfor i in range(10000):\n\
+            t = repeat('func()', 'from __main__ import func', number=100, repeat=500)\n"
+        self.assertEqual(run_code(code), '计算超时,请简化你的代码\n运行时间不得超过 '+str(timeOut)+' 秒')
--- start.sh
+++
@@ -1,2 +0,0 @@
-﻿# 启动服务
-python3 manage.py runserver
