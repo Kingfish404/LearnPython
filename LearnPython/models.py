acb19002c8eef997eff6b6ff02f9cb43db8b4cc2
Kingfish404 <jinyu929@qq.com>
2020-04-28 10:41:58 +0800

    update run_code

--- LearnPython/models.py
+++ LearnPython/models.py
@@ -10,17 +10,22 @@ from django.views.decorators.csrf import csrf_exempt
 
 def run_code(code):
     # 在服务器上运行代码
+    div = code.split(sep="\n")
+    # 如果输入的代码只是一行表达式，那就直接输出计算结果
+    if(len(div)==1 and not 'print' in div[0] and not 'import' in div[0]):
+        code = 'print('+code+')'
+        print("edit:",code)
     try:
         output = subprocess.check_output(
-            ['python', '-c', code], universal_newlines=True, stderr=subprocess.STDOUT)
+            ['python', '-c', code], universal_newlines=True, stderr=subprocess.STDOUT,timeout=2)
     except Exception as e:
         output = e.output
     return output
 
-
 @csrf_exempt
 @require_POST
 def api(request):
+    # 解析代码的api
     code = request.POST.get('code')
     output = run_code(code)
     return JsonResponse({'output': output})
