d64a8d9e929174cc53883a10966bf2b446cd277d
Kingfish404 <jinyu929@qq.com>
2020-05-17 19:28:21 +0800

    add svg icon and fix adapt

--- LearnPython/tests.py
+++ LearnPython/tests.py
@@ -10,20 +10,20 @@ class models_api_Test(TestCase):
     def test_self(self):
         code = '2**10'
         result = '1024'
-        self.assertEqual(run_code(code), result)
+        self.assertEqual(run_code(code).output, result)
 
     def test_cmd(self):
-        self.assertEqual(run_code('1+1'), '2')
+        self.assertEqual(run_code('1+1').output, '2')
 
     def test_print(self):
-        self.assertEqual(run_code('print(2**10)'), '1024')
+        self.assertEqual(run_code('print(2**10)').output, '1024')
 
     def test_empty(self):
-        self.assertEqual(run_code('  '), '请输入代码')
+        self.assertEqual(run_code('  ').output, '请输入代码')
 
     def test_timeout(self):
         code = "from timeit import repeat\ndef func():\n\
             s = 0\nfor i in range(10000):\n\
             t = repeat('func()', 'from __main__ import func', number=100, repeat=500)\n"
         self.assertEqual(
-            run_code(code), '计算超时,请简化你的代码\n运行时间不得超过 '+str(timeOut)+' 秒')
+            run_code(code).output, '计算超时,请简化你的代码\n运行时间不得超过 '+str(timeOut)+' 秒')
